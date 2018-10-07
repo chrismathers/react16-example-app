47f9a8bbe5c14212fc426fd7e3e558babe1fd6fb
chris mathers <xee@xemedia.com>
2018-10-06 23:49:16 -0400

    Updates

---
+++ precache-manifest.e8c54ba6c950c23f315fdc8df7936053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chrismathers/react16-example-app.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f7a947263b79734c194",
+    "url": "/chrismathers/react16-example-app.git/static/js/runtime~main.8f7a9472.js"
+  },
+  {
+    "revision": "cac823163a442439141f",
+    "url": "/chrismathers/react16-example-app.git/static/js/main.cac82316.chunk.js"
+  },
+  {
+    "revision": "4192797bad3e23b2fc18",
+    "url": "/chrismathers/react16-example-app.git/static/js/1.4192797b.chunk.js"
+  },
+  {
+    "revision": "cac823163a442439141f",
+    "url": "/chrismathers/react16-example-app.git/static/css/main.971df317.chunk.css"
+  },
+  {
+    "revision": "395b5135a65532ea2d7ad0c79d26059a",
+    "url": "/chrismathers/react16-example-app.git/index.html"
+  }
+];
